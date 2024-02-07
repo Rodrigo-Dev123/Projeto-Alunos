bee7a7ec24db85b04f3057bd1c0aac8412f5a8cf
Rodrigo <rodrigojsg9@gmail.com>
2024-02-07 18:46:43 -0300

    firts resquest

--- Front/src/components/Alunos/index.jsx
+++ Front/src/components/Alunos/index.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import axios from "../../services/axios";
+
 function Alunos() {
+    useEffect(() => {
+        async function fetchData() {
+            const data = await axios.get('/alunos')
+                .then(response => response);
+            console.log(data);
+        }
+
+        fetchData();
+    }, []);
+
    return (
     <div className="App">
         <h1>Alunos</h1>
